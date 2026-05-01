fa5fb2b5370bb5cbebbbee991c99d1e668c0b537
rgiri1706 <rgiri1706@gmail.com>
2026-05-01 15:49:41 +0530

    fix for alignment

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,8 +3,8 @@ import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/in
 
 export function Header() {
     return (
-        <div className="sticky top-0 z-50 flex border-b px-6 py-3.5 bg-background justify-between items-center">
-            <InputGroup className="w-1/2 rounded-full pl-4">
+        <div className="sticky top-0 z-50 flex border-b px-6 py-3.5 bg-background items-center">
+            <InputGroup className="w-1/2 rounded-full pl-4 mx-auto">
                 <InputGroupAddon align="inline-start">
                     <Search />
                 </InputGroupAddon>
