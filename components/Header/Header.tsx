bb0b3b7b1d71f9455bcf22ad95139dd5ba305a92
rgiri1706 <rgiri1706@gmail.com>
2026-05-01 15:47:02 +0530

    sidebar and header

---
+++ components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { Bell, Search } from "lucide-react";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+
+export function Header() {
+    return (
+        <div className="sticky top-0 z-50 flex border-b px-6 py-3.5 bg-background justify-between items-center">
+            <InputGroup className="w-1/2 rounded-full pl-4">
+                <InputGroupAddon align="inline-start">
+                    <Search />
+                </InputGroupAddon>
+                <InputGroupInput
+                    placeholder="Search for topics, feelings, support...."
+                    className="placeholder:font-semibold text-black rounded-full"
+                />
+            </InputGroup>
+            <Bell size={20} />
+        </div>
+    );
+}
